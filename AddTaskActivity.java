631bc550562284685b44f25a168143cd6364bf1c
ymlyy <114065364+ymlyy@users.noreply.github.com>
2023-05-10 13:44:39 +0800

    1020064

---
+++ AddTaskActivity.java
@@ -0,0 +1,161 @@
+
+package com.skapps.android.todolist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import com.skapps.android.todolist.database.AppDatabase;
+import com.skapps.android.todolist.database.TaskEntry;
+import java.util.Date;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    // Extra for the task ID to be received in the intent
+    public static final String EXTRA_TASK_ID = "extraTaskId";
+    // Extra for the task ID to be received after rotation
+    public static final String INSTANCE_TASK_ID = "instanceTaskId";
+    // Constants for priority
+    public static final int PRIORITY_HIGH = 1;
+    public static final int PRIORITY_MEDIUM = 2;
+    public static final int PRIORITY_LOW = 3;
+    // Constant for default task id to be used when not in update mode
+    private static final int DEFAULT_TASK_ID = -1;
+    // Constant for logging
+    private static final String TAG = AddTaskActivity.class.getSimpleName();
+    // Fields for views
+    EditText mEditText;
+    RadioGroup mRadioGroup;
+    Button mButton;
+
+    private int mTaskId = DEFAULT_TASK_ID;
+
+    private AppDatabase mDb;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        initViews();
+
+         mDb = AppDatabase.getInstance(getApplicationContext());
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(INSTANCE_TASK_ID)) {
+            mTaskId = savedInstanceState.getInt(INSTANCE_TASK_ID, DEFAULT_TASK_ID);
+        }
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(EXTRA_TASK_ID)) {
+            mButton.setText(R.string.update_button);
+            setTitle("Update Task");
+            if (mTaskId == DEFAULT_TASK_ID) {
+                mTaskId = intent.getIntExtra(EXTRA_TASK_ID, DEFAULT_TASK_ID);
+
+                AddTaskViewModelFactory factory = new AddTaskViewModelFactory(mDb, mTaskId);
+                final AddTaskViewModel viewModel = new ViewModelProvider(this, factory).get(AddTaskViewModel.class);
+                viewModel.getTask().observe(this, new Observer<TaskEntry>() {
+                    @Override
+                    public void onChanged(TaskEntry taskEntry) {
+                        viewModel.getTask().removeObserver(this);
+                        populateUI(taskEntry);
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(INSTANCE_TASK_ID, mTaskId);
+        super.onSaveInstanceState(outState);
+    }
+
+
+    private void initViews() {
+        mEditText = findViewById(R.id.editTextTaskDescription);
+        mRadioGroup = findViewById(R.id.radioGroup);
+
+        mButton = findViewById(R.id.saveButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSaveButtonClicked();
+            }
+        });
+    }
+
+
+    private void populateUI(TaskEntry task) {
+        if (task == null ){
+            return;
+        }
+        mEditText.setText(task.getDescription());
+        setPriorityInViews(task.getPriority());
+    }
+
+
+    public void onSaveButtonClicked() {
+        String description = mEditText.getText().toString();
+        int priority = getPriorityFromViews();
+        Date date = new Date();
+
+        if(description.equals("")){
+            Toast.makeText(this,"Describe your task", Toast.LENGTH_SHORT).show();
+        }else{
+            final TaskEntry taskEntry = new TaskEntry(description, priority, date, false );
+
+            AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                @Override
+                public void run() {
+                    if(mTaskId == DEFAULT_TASK_ID){
+                        mDb.taskDao().insertTask(taskEntry);
+                    }else {
+                        taskEntry.setId(mTaskId);
+                        mDb.taskDao().updateTask(taskEntry);
+                    }
+                    finish(); //automatically return to main activity
+                }
+            });
+        }
+
+
+    }
+
+
+    public int getPriorityFromViews() {
+        int priority = 1;
+        int checkedId = ((RadioGroup) findViewById(R.id.radioGroup)).getCheckedRadioButtonId();
+        switch (checkedId) {
+            case R.id.radButton1:
+                priority = PRIORITY_HIGH;
+                break;
+            case R.id.radButton2:
+                priority = PRIORITY_MEDIUM;
+                break;
+            case R.id.radButton3:
+                priority = PRIORITY_LOW;
+        }
+        return priority;
+    }
+
+
+    public void setPriorityInViews(int priority) {
+        switch (priority) {
+            case PRIORITY_HIGH:
+                ((RadioGroup) findViewById(R.id.radioGroup)).check(R.id.radButton1);
+                break;
+            case PRIORITY_MEDIUM:
+                ((RadioGroup) findViewById(R.id.radioGroup)).check(R.id.radButton2);
+                break;
+            case PRIORITY_LOW:
+                ((RadioGroup) findViewById(R.id.radioGroup)).check(R.id.radButton3);
+        }
+    }
+}
